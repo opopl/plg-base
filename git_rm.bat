56ad2342b0b1365d92dc6a3d36cccf58809afc56
op <op@oppc>
2016-04-19 08:51:20 +0300

    u

---
+++ git_rm.bat
@@ -0,0 +1,6 @@
+git rm autoload/base.vim.tags
+git rm autoload/base/complete.vim.tags
+git rm autoload/base/file.vim.tags
+git rm autoload/base/init.vim.tags
+git rm autoload/base/stl.vim.tags
+git rm autoload/base/tg.vim.tags
