b4efe265f6af87061529072f16d49caab5e00a4b
op <op@localhost>
2020-11-29 17:54:58 +0200

    u

--- perl/lib/Base/Scripts/PPZ.pm
+++ perl/lib/Base/Scripts/PPZ.pm
@@ -92,17 +92,86 @@ sub dhelp {
     return $self;   
 }
 
+sub load_f_ppi {
+    my ($self, $file) = @_;
+
+    my $doc = PPI::Document->new($file);
+
+    $doc->index_locations;
+
+    my $f = sub { 
+        $_[1]->isa( 'PPI::Statement::Sub' ) 
+        || $_[1]->isa( 'PPI::Statement::Package' )
+        || $_[1]->isa( 'PPI::Statement::Variable' )
+        || $_[1]->isa( 'PPI::Statement::Include' )
+    };
+    my @nodes = @{ $doc->find( $f ) || [] };
+
+    my( $ns, $subname_short, $subname_full );
+    my $add = {};
+    $add->{$_} = 1 for(qw(vars subs packs));
+
+    for my $node (@nodes){
+
+        #$node_count++;
+        #last if ( $max_node_count && ( $node_count == $max_node_count ) );
+
+###PPI_Statement_Sub
+        $node->isa( 'PPI::Statement::Sub' ) && do { 
+            next unless $add->{subs};
+
+            $ns ||= 'main'; 
+            $subname_full  = $ns . '::' . $node->name;
+            $subname_short = $node->name; 
+
+        };
+###PPI_Statement_Variable
+        $node->isa( 'PPI::Statement::Variable' ) && do { 
+            next unless $add->{vars};
+
+            my $type = $node->type;
+            next unless $type eq 'our';
+
+            my @a = ($ns,$file,$type);
+
+            my $vars = [ $node->variables ];
+        };
+###PPI_Statement_Package
+        $node->isa( 'PPI::Statement::Package' ) && do { 
+            $ns = $node->namespace; 
+
+            next unless $add->{packs};
+        };
+    }
+
+    return $self;   
+
+}
+
 sub load_f {
     my ($self, $ref) = @_;
     $ref ||= {};
 
     my $file = $ref->{file} || $self->{file} || '';
 
-    my $module = $self->{module};
-    my @libs;
+    if ($file) {
+        $self->load_f_ppi($file);
+    }else{
+        my $module = $self->{module};
+        if ($module) {
+            my @libs;
+        
+            my @data = list_pm_files($module,@libs);
+        
+            foreach my $item (@data) {
+                my $file = $item->{path};
+                next unless $file;
 
-    my @files = list_pm_files($module,@libs);
-    print Dumper(\@files) . "\n";
+                $self->load_f({ file => $file });
+            }
+            return $self;
+        }
+    }
 
     return $self;
 }
