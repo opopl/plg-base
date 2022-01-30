145b75ece933a330d2dfd54156b4575c7db6c113
op <op@localhost>
2022-01-30 13:05:27 +0200

    u

--- perl/lib/Base/Data.pm
+++ perl/lib/Base/Data.pm
@@ -8,11 +8,11 @@ use warnings;
 binmode STDOUT,':encoding(utf8)';
 
 use Deep::Hash::Utils qw(
-	deepvalue
+    deepvalue
 );
 use Base::String qw(
-	str_split
-	str_split_sn
+    str_split
+    str_split_sn
 );
 
 use Exporter ();
@@ -35,39 +35,46 @@ my @ex_vars_array=qw(
 
 %EXPORT_TAGS = (
 ###export_funcs
-	'funcs' => [qw( 
-		d_str_split
-		d_str_split_sn
-		d_path
-	)],
-	'vars'  => [ @ex_vars_scalar,@ex_vars_array,@ex_vars_hash ]
+    'funcs' => [qw(
+        d_str_split
+        d_str_split_sn
+        d_path
+    )],
+    'vars'  => [ @ex_vars_scalar,@ex_vars_array,@ex_vars_hash ]
 );
 
 @EXPORT_OK = ( @{ $EXPORT_TAGS{'funcs'} }, @{ $EXPORT_TAGS{'vars'} } );
 
 sub d_path {
-	my ($data, $path, $default) = @_;
+    my ($data, $path, $default) = @_;
 
-	my @path = split(' ',$path);
-	my $val = deepvalue($data,@path) // $default;
-	return $val;
+    my @path = split(' ',$path);
+    my $val = deepvalue($data,@path) // $default;
+    return $val;
 }
 
 sub d_str_split {
-	my ($data, $path, $ref) = @_;
-	$ref ||= {};
+    my ($data, $path, $ref) = @_;
+    $ref ||= {};
 
-	my $val = d_path($data,$path,'');
-	my @s = str_split($val,$ref);
-	return @s;
+    my $val = d_path($data,$path,'');
+    my @s = str_split($val,$ref);
+    return @s;
 }
 
 sub d_str_split_sn {
-	my ($data, $path) = @_;
+    my ($data, $path) = @_;
 
-	my $val = d_path($data,$path,'');
-	my @s = str_split_sn($val);
-	return @s;
+    my $val = d_path($data,$path,'');
+    my @s;
+    unless (ref $val) {
+        push @s, str_split_sn($val);
+    }elsif(ref $val eq 'ARRAY'){
+        foreach my $x (@$val) {
+            push @s, str_split_sn($x);
+        }
+    }
+    return @s;
 }
 
 1;
